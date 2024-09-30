96e1636df2bfb90c1c85ac03e56a927505746dde
Yann7c <kemdjouyann@gmail.com>
2024-09-30 09:09:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
+global.ownernomer = "237680866295"
 global.ownername = "Qasim Ali"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '237680866295'  //creator number
+global.ownername = 'King Maurel v8' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
